98a91c785abb83cfa39ee1ccc14bac9311a31ab2
Dmitriy Aloyts <dmitriy@iinet.net.au>
2020-01-18 22:20:00 +1100

    working version

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger) VALUES ('Fish');
+INSERT INTO burgers (burger) VALUES ('Vegi');
+INSERT INTO burgers (burger, eaten) VALUES ('Chicken', true);
+INSERT INTO burgers (burger, eaten) VALUES ('Tea', true);
+INSERT INTO burgers (burger, eaten) VALUES ('Salt', true);
