4c14c6eabeedbcc8b08919a725dc36f37219ae58
Dmitriy Aloyts <dmitriy@iinet.net.au>
2020-02-10 11:05:12 +1100

    added new code front end

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-INSERT INTO burgers (burger) VALUES ('Fish');
-INSERT INTO burgers (burger) VALUES ('Vegi');
-INSERT INTO burgers (burger, eaten) VALUES ('Chicken', true);
-INSERT INTO burgers (burger, eaten) VALUES ('Tea', true);
-INSERT INTO burgers (burger, eaten) VALUES ('Salt', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
