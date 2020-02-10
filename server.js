504f636e9f27243465b44ac59914d669b53b1a5d
Dmitriy Aloyts <dmitriy@iinet.net.au>
2020-02-10 16:59:54 +1100

    removed timeout

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 var express = require("express");
-var timeout = require('connect-timeout');
-
 
 var PORT = process.env.PORT || 8000;
 var app = express();
@@ -11,12 +9,6 @@ app.use(express.static("public"));
 // Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(timeout(15000));
-app.use(haltOnTimedout);
-
-function haltOnTimedout(req, res, next) {
-    if (!req.timedout) next();
-}
 
 var exphbs = require("express-handlebars");
 
